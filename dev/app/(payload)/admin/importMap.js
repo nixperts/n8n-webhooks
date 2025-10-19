feff1088c3be9b867a5c83b4dd4ad1c2863e75d2
NixpertAlex <nixperts@proton.me>
2025-10-19 06:49:04 +0200

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'n8n-webhooks/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'n8n-webhooks/rsc'
+
+export const importMap = {
+  'n8n-webhooks/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'n8n-webhooks/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
